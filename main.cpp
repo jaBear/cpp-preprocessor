d120f30d8d29321b368a50ed96f25823cc977f7d
jaBear <hq16@yandex.ru>
2024-02-12 20:01:57 +0300

    Praktikum Sprint 5

--- main.cpp
+++ main.cpp
@@ -14,8 +14,11 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+string GetLine(istream& in) {
+    string s;
+    getline(in, s);
+    return s;
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
@@ -23,6 +26,84 @@ string GetFileContents(string file) {
     // конструируем string по двум итераторам
     return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
 }
+
+// напишите эту функцию
+bool PrepFunc(const path& in_file, ofstream& out_file, const vector<path>& include_directories) {
+    static regex num_reg(R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
+    static regex num_reg2(R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
+    int line_number = 0;
+    if(filesystem::exists(in_file)) {
+        ifstream stream;
+        stream.open(in_file);
+        string line;
+            while (getline(stream, line)) {
+                line_number++;
+                smatch m;
+                
+                cout << line << endl;
+                
+                if (regex_match(line, m, num_reg)) {
+                    path new_path = in_file.parent_path() / path(m[1]);
+                    fstream fs(new_path);
+                    
+                    if (fs.is_open()) {
+                        PrepFunc(new_path, out_file, include_directories);
+                        continue;
+                    } else {
+                        bool res = false;
+                        path new_path2;
+                        for (auto d : include_directories) {
+                            fstream fs1(d / path(m[1]));
+                            if (fs1.is_open()) {
+                                new_path2 = d / path(m[1]);
+                                res = true;
+                            }
+                        }
+                        if (res) {
+                            res = false;
+                            PrepFunc(new_path2, out_file, include_directories);
+                            continue;
+                        } else
+                        cout << "unknown include file "s << path(m[1]).filename().string() << " at file "s << path(in_file).string() << " at line "s << line_number << endl;
+                        return false;
+                    }
+                    
+                } else if(regex_match(line, m, num_reg2)) {
+                    bool res = false;
+                    path new_path2;
+                    for (auto d : include_directories) {
+                        fstream fs1(d / path(m[1]));
+                        if (fs1.is_open()) {
+                            new_path2 = d / path(m[1]);
+                            res = true;
+                        }
+                    }
+                    if (res) {
+                        res = false;
+                        PrepFunc(new_path2, out_file, include_directories);
+                        continue;
+                    } else
+                        cout << "unknown include file "s << path(m[1]).filename().string() << " at file "s << path(in_file).string() << " at line "s << line_number << endl;
+                    return false;
+                    
+                } else {
+                    out_file << line << '\n';
+                }
+            };
+    } else {
+        return false;
+    }
+
+    return true;
+
+};
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    ofstream out(out_file, ios::app);
+    
+    return PrepFunc(in_file, out, include_directories);
+}
+
 
 void Test() {
     error_code err;
@@ -69,6 +150,7 @@ void Test() {
         ofstream file("sources/include2/lib/std2.h");
         file << "// std2\n"s;
     }
+
 
     assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
                                   {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
